337f39ec10dee583a312f03079bead4eaf7e1666
Hamza <70992074+Hamzawhitehatjr@users.noreply.github.com>
2021-01-11 20:31:30 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+function preload()
+{
+  backgroundImg=loadImage("doglmg/bg.png");
+  backgroundImg=loadImage("doglmg1/bg.png");
+
+
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+background(46,139,87)
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
